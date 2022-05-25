8117d7bab3479d0349d7ea3b73d19c4dccb2e4fd
MathisDevs <mathisaud.pro@gmail.com>
2022-05-25 23:20:46 +0200

    Add user add & user remove

--- src/main/java/fr/mathis/mm/utils/enums/Lang.java
+++ src/main/java/fr/mathis/mm/utils/enums/Lang.java
@@ -4,11 +4,13 @@ import fr.mathis.api.message.Color;
 
 public enum Lang {
 
-    MAINTENANCE_ENABLED(Color.RED + "La maintenance est activé."),
-    MAINTENANCE_DISABLED(Color.RED + "La maintenance est désactivé."),
+    MAINTENANCE_ENABLED(Color.GREEN + "La maintenance est activé."),
+    MAINTENANCE_DISABLED(Color.GREEN + "La maintenance est désactivé."),
 
-    PLAYER_MAINTENANCE_KICK(Color.RED + "Le serveur est en phase de maintenance."),
-    MAINTENANCE_COMMAND_INVALID(Color.RED + "Commande de maintenance incconu! (/maintenance help).");
+    PLAYER_MAINTENANCE_KICK(Color.RED + "Le serveur rentre en maintenance. Retrouvez plus d'informations sur nos réseaux. Nous revenons au plus rapidement."),
+    MAINTENANCE_COMMAND_INVALID(Color.RED + "Commande de maintenance incconu! (/maintenance help)."),
+    USER_ADD(Color.GREEN + "Utilisateur ajouté à la withelist."),
+    USER_REMOVE(Color.GREEN + "Utilisateur retiré de la withelist.");
 
     private final String lang;
 

176c8fe2b05beda0b1c6d09656e3a733148305a3
MathisDevs <mathisaud.pro@gmail.com>
2022-05-25 23:41:19 +0200

    In production

--- src/main/java/fr/mathis/mm/commands/MaintenanceCommand.java
+++ src/main/java/fr/mathis/mm/commands/MaintenanceCommand.java
@@ -5,9 +5,14 @@ import fr.mathis.api.utils.enums.Perm;
 import fr.mathis.mm.manager.ServerManagement;
 import fr.mathis.mm.mMaintenance;
 import fr.mathis.mm.utils.enums.Lang;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
 
 public class MaintenanceCommand implements CommandExecutor {
     public MaintenanceCommand(mMaintenance plugin) {
@@ -23,34 +28,58 @@ public class MaintenanceCommand implements CommandExecutor {
         if(label.equalsIgnoreCase("maintenance")) {
 
             if(args.length == 1) {
-                //Enable Maintenance
                 if(args[0].equalsIgnoreCase("on")) {
                     mMaintenance.Enable = true;
                     ServerManagement.kickAllNotAllowed();
                     commandSender.sendMessage(Lang.MAINTENANCE_ENABLED.getLang());
 
-                 //Disable maintenance
                 } else if(args[0].equalsIgnoreCase("off")) {
                     mMaintenance.Enable = false;
                     commandSender.sendMessage(Lang.MAINTENANCE_DISABLED.getLang());
 
-                 //Get help
                 } else if(args[0].equalsIgnoreCase("help")) {
                     commandSender.sendMessage(Line.chatLine());
                     commandSender.sendMessage("/maintenance on / off");
                     commandSender.sendMessage(Line.chatLine());
 
-                 //Add player
-                } else if(args[0].equalsIgnoreCase("add")) {
-
-                  //Remove player
-                } else if(args[0].equalsIgnoreCase("remove")) {
-
-                  //Player list
                 } else if(args[0].equalsIgnoreCase("list")) {
-
+                    OfflinePlayer player;
+                    for(UUID uuid: mMaintenance.getAuthorized()) {
+                        player = Bukkit.getOfflinePlayer(uuid);
+                        if(player != null) {
+                            commandSender.sendMessage(Line.chatLine());
+                            commandSender.sendMessage("§a+ §r" + player.getName());
+                            commandSender.sendMessage(Line.chatLine());
+                        } else {
+                            commandSender.sendMessage(Line.chatLine());
+                            commandSender.sendMessage("§cAucun joueur enregistré dans la withlist.");
+                            commandSender.sendMessage(Line.chatLine());
+                        }
+                    }
                 }
+            } else if(args.length == 2) {
+                if(args[0].equalsIgnoreCase("add")) {
+                    final String playerName = args[1];
+                    final Player player = Bukkit.getPlayerExact(playerName);
+                    if(player != null) {
+                        final UUID uuid = player.getUniqueId();
+                        mMaintenance.getAuthorized().add(uuid);
+                        commandSender.sendMessage(Lang.USER_ADD.getLang());
+                    } else {
+                        commandSender.sendMessage("§cLe joueur n'est actuellement pas connecté.");
+                    }
 
+                } else if(args[0].equalsIgnoreCase("remove")) {
+                    final String playerName = args[1];
+                    final Player player = Bukkit.getPlayerExact(playerName);
+                    if(player != null) {
+                        final UUID uuid = player.getUniqueId();
+                        mMaintenance.getAuthorized().remove(uuid);
+                        commandSender.sendMessage(Lang.USER_REMOVE.getLang());
+                    } else {
+                        commandSender.sendMessage("§cLe joueur n'est actuellement pas connecté.");
+                    }
+                }
             }
         } else {
             commandSender.sendMessage(Lang.MAINTENANCE_COMMAND_INVALID.getLang());
